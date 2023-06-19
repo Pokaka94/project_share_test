f05c70ef15e71651b24993ef3e65a4380e6a0894
jongin <euncun9810@gmail.com>
2023-06-19 11:32:02 +0900

    first commit

---
+++ githubtest-1/src/test/java/com/example/ai/Githubtest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Githubtest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
